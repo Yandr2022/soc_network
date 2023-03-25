f1895ba4e33a02c0315146344b0b5d29b2907f04
Yandr2022 <sharkievich@gmail.com>
2023-03-25 19:19:41 +0300

    generated tables to DB

--- src/main/java/com/itstep/yuliandr/socNtW/soc_network/entity/User.java
+++ src/main/java/com/itstep/yuliandr/socNtW/soc_network/entity/User.java
@@ -1,25 +1,45 @@
 package com.itstep.yuliandr.socNtW.soc_network.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.itstep.yuliandr.socNtW.soc_network.entity.enums.ERole;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(unique = true, updatable = false)
     private String username;
+    @Column(nullable = false)
     private String lastname;
+    @Column(unique = true)
     private String email;
+    @Column(columnDefinition = "text")
     private String bio;
+    @Column(length = 3000)
     private String password;
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_Id"))
     private Set<ERole> roles = new HashSet<>();
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createdDate;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
     @PrePersist
     protected void onCreate(){
         this.createdDate = LocalDateTime.now();
