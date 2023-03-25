67f4b749bd5760de3e24a1d20dd873934e9a4d37
Yandr2022 <sharkievich@gmail.com>
2023-03-25 19:45:53 +0300

    added repositories (JPA)

---
+++ src/main/java/com/itstep/yuliandr/socNtW/soc_network/repository/CommentRepository.java
@@ -0,0 +1,21 @@
+package com.itstep.yuliandr.socNtW.soc_network.repository;
+
+import com.itstep.yuliandr.socNtW.soc_network.entity.Comment;
+import com.itstep.yuliandr.socNtW.soc_network.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+
+    List<Comment> findAllByPost(Post post);
+
+    Comment findByIdAndUserId(Long commentId, Long userId);
+
+
+
+}
