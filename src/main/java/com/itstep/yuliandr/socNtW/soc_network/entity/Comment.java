c1b80faee97dc0f3109452c572a396de01dcd939
Yandr2022 <sharkievich@gmail.com>
2023-03-25 18:48:38 +0300

    added fields for entities

--- src/main/java/com/itstep/yuliandr/socNtW/soc_network/entity/Comment.java
+++ src/main/java/com/itstep/yuliandr/socNtW/soc_network/entity/Comment.java
@@ -1,4 +1,28 @@
 package com.itstep.yuliandr.socNtW.soc_network.entity;
 
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+
 public class Comment {
+    private Long id;
+
+    private Post post;
+
+    private String username;
+
+    private Long userId;
+
+
+    private String message;
+
+
+    private LocalDateTime createdDate;
+
+    public Comment() {
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }
--- src/main/java/com/itstep/yuliandr/socNtW/soc_network/entity/Image.java
+++ src/main/java/com/itstep/yuliandr/socNtW/soc_network/entity/Image.java
@@ -1,4 +1,17 @@
 package com.itstep.yuliandr.socNtW.soc_network.entity;
 
 public class Image {
+
+    private Long id;
+
+    private String name;
+
+
+    private byte[] imageBytes;
+
+
+    private Long userId;
+
+
+    private Long postId;
 }
