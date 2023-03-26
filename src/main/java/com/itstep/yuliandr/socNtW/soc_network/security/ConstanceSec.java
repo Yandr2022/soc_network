dc6e99a3f1533c6f4473c99c9b7a1b03e9f89eb7
Yandr2022 <sharkievich@gmail.com>
2023-03-26 18:00:35 +0300

    added requests objects for transfer

--- src/main/java/com/itstep/yuliandr/socNtW/soc_network/security/ConstanceSec.java
+++ src/main/java/com/itstep/yuliandr/socNtW/soc_network/security/ConstanceSec.java
@@ -4,7 +4,7 @@ public class ConstanceSec {
 
 public static final String SIGN_UP_PATH="api/auth/**";//путь к авторизации
     public static final String KEY = "SecretKeyGenJWT";//ключ для шифрования токена
-    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String TOKEN_PREFIX = "Bearer ";//на предъявителя
     public static final String HEADER_STRING = "Authorization";
     public static final String CONTENT_TYPE = "application/json";
     public static final long EXPIRATION_TIME = 900_000; //15 мин до обновления токена - log out
