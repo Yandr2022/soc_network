ada522e7605391afea65016adc7bbc4409c73e06
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-04-05 17:28:18 +0300

    added methods for update user

--- src/main/java/com/itstep/yuliandr/socNtW/soc_network/service/UserService.java
+++ src/main/java/com/itstep/yuliandr/socNtW/soc_network/service/UserService.java
@@ -1,5 +1,6 @@
 package com.itstep.yuliandr.socNtW.soc_network.service;
 
+import com.itstep.yuliandr.socNtW.soc_network.dto.UserDTO;
 import com.itstep.yuliandr.socNtW.soc_network.repository.UserRepository;
 import com.itstep.yuliandr.socNtW.soc_network.entity.User;
 import com.itstep.yuliandr.socNtW.soc_network.entity.enums.ERole;
@@ -8,8 +9,11 @@ import com.itstep.yuliandr.socNtW.soc_network.payload.request.SignUpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.Principal;
 
 @Service
 public class UserService {
@@ -40,5 +44,24 @@ public class UserService {
             throw new UserExistException("The user " + user.getUsername() + " already exist. Please try again");//custom exception
 
         }
+    }
+
+    public User updateUser(UserDTO userDTO, Principal principal){//principal -содержит данные пользователя из БД
+        User user = getUserByPrincipal(principal);
+        user.setName(userDTO.getUsername());
+        user.setLastname(userDTO.getLastname());
+        user.setUsername(userDTO.getUsername());
+        user.setBio(userDTO.getBio());
+     return repository.save(user);
+    }
+
+    public User getCurrentUser(Principal principal){
+        return getUserByPrincipal(principal);
+    }
+
+    public User getUserByPrincipal(Principal principal){
+        String username = principal.getName();
+        return repository.findUserByUsername(username).orElseThrow(() -> new UsernameNotFoundException
+                ("User with username " + username + " not found "));
     }
 }
