a0422c96b8151cad191b804ef1543c734909603f
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-04-05 13:25:34 +0300

    IXED AUTHORIZATION PATH ERROR

--- src/main/java/com/itstep/yuliandr/socNtW/soc_network/security/ConstanceSec.java
+++ src/main/java/com/itstep/yuliandr/socNtW/soc_network/security/ConstanceSec.java
@@ -2,7 +2,7 @@ package com.itstep.yuliandr.socNtW.soc_network.security;
 
 public class ConstanceSec {
 
-public static final String SIGN_UP_PATH="api/auth/**";//путь к авторизации
+public static final String SIGN_UP_PATH="/api/auth/*";//путь к авторизации
     public static final String KEY = "SecretKeyGenJWT";//ключ для шифрования токена
     public static final String TOKEN_PREFIX = "Bearer ";//на предъявителя
     public static final String HEADER_STRING = "Authorization";
